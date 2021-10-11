0e05e826a28f2866105ce28014973dc82c96a0a8
Augustin Debureaux <adebureaux@gmail.com>
2021-10-11 11:36:52 +0200

    parsing

--- philo.c
+++ philo.c
@@ -6,44 +6,58 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 15:20:22 by adeburea          #+#    #+#             */
-/*   Updated: 2021/10/07 12:31:21 by adeburea         ###   ########.fr       */
+/*   Updated: 2021/10/11 11:33:17 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-char	*parse_philo(int ac, char **av, t_philo *philo)
+char	*parse_game(int ac, char **av, t_game *game)
 {
 	if (ac != 5 && ac != 6)
 		return ("must have 4 or 5 arguments");
-	philo->number = ft_atoi(*(av + 1));
-	if (philo->number < 0)
-		return ("arg 1 must be 'number_of_philosophers'");
-	philo->die = ft_atoi(*(av + 2));
-	if (philo->die < 1)
+	game->number = ft_atoi(*(av + 1));
+	if (game->number < 0)
+		return ("arg 1 must be 'number_of_gamesophers'");
+	game->die = ft_atoi(*(av + 2));
+	if (game->die < 1)
 		return ("arg 2 must be 'time_to_die'");
-	philo->eat = ft_atoi(*(av + 3));
-	if (philo->eat < 1)
+	game->eat = ft_atoi(*(av + 3));
+	if (game->eat < 1)
 		return ("arg 3 must be 'time_to_eat'");
-	philo->sleep = ft_atoi(*(av + 4));
-	if (philo->sleep < 1)
+	game->sleep = ft_atoi(*(av + 4));
+	if (game->sleep < 1)
 		return ("arg 4 must be 'time_to_sleep'");
-	philo->times = -1;
+	game->times = -1;
 	if (ac == 6)
-		philo->times = ft_atoi(*(av + 5));
-	if (ac == 6 && philo->times < 1)
-		return ("arg 5 must be 'number_of_times_each_philosopher_must_eat'");
+		game->times = ft_atoi(*(av + 5));
+	if (ac == 6 && game->times < 1)
+		return ("arg 5 must be 'number_of_times_each_gamesopher_must_eat'");
 	return (NULL);
+}
+
+void *myThreadFun(void *vargp)
+{
+    sleep(1);
+    printf("Printing GeeksQuiz from Thread \n");
+    return NULL;
+}
+
+void init_game(t_game *game)
+{
+    pthread_t thread_id;
+    printf("Before Thread\n");
+    pthread_create(&thread_id, NULL, myThreadFun, NULL);
+    pthread_join(thread_id, NULL);
+    printf("After Thread\n");
 }
 
 int	main(int ac, char **av)
 {
-	t_philo	philo;
+	t_game	game;
 
-	if (parse_philo(ac, av, &philo))
-		return (printf("%s: error: %s\n", av[0], parse_philo(ac, av, &philo)));
-	struct timeval current_time;
-	gettimeofday(&current_time, NULL);
-	printf("seconds : %ld\nmicro seconds : %d\n", current_time.tv_sec, current_time.tv_usec);
+	if (parse_game(ac, av, &game))
+		return (printf("%s: error: %s\n", av[0], parse_game(ac, av, &game)));
+	init_game(&game);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 15:20:23 by adeburea          #+#    #+#             */
-/*   Updated: 2021/10/07 12:30:14 by adeburea         ###   ########.fr       */
+/*   Updated: 2021/10/11 11:32:47 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,22 @@
 # include <sys/time.h>
 # include <pthread.h>
 
-typedef struct s_philo
+typedef struct s_game
 {
 	int	number;
 	int	die;
 	int	eat;
 	int	sleep;
 	int	times;
+}			t_game;
+
+typedef struct s_philo
+{
+	int	is_die;
+	int	is_eat;
+	int	is_sleep;
+	int	l_fork;
+	int	r_fork;
 }			t_philo;
 
 int		ft_isdigit(int c);
