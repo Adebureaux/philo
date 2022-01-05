5493500ae892580f5207af8852809ef393780bcd
Augustin Debureaux <adebureaux@gmail.com>
2022-01-05 20:11:14 +0100

    WIP : philo dead

--- routine.c
+++ routine.c
@@ -6,7 +6,7 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 15:35:03 by adeburea          #+#    #+#             */
-/*   Updated: 2022/01/05 17:11:28 by adeburea         ###   ########.fr       */
+/*   Updated: 2022/01/05 20:09:54 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,14 @@
 void	philo_speak(t_board *board, t_philo *philo, char *str, int id)
 {
 	pthread_mutex_lock(philo[id].print);
-	if (!board->stop)
-	{
-			ft_putstr(philo[id].color);
-		ft_putnbr(get_time() - board->start_time);
-		write(1, " ", 1);
-		ft_putnbr((size_t)id + 1);
-		write(1, " ", 1);
-		ft_putstr(str);
-		ft_putstr("\033[0m");
-	}
+	ft_putstr(philo[id].color);
+	ft_putnbr(get_time() - board->start_time);
+	write(1, " ", 1);
+	ft_putnbr((size_t)id + 1);
+	write(1, " ", 1);
+	ft_putstr(str);
+	ft_putstr("\033[0m");
 	pthread_mutex_unlock(philo[id].print);
-}
-
-int		check_meal(t_board *board, t_philo *philo)
-{
-	int	i;
-	int	count;
-
-	i = -1;
-	count = 0;
-	while (++i < board->number)
-	{
-		if (philo[i].count_meal >= board->limit)
-			count++;
-	}
-	if (count == board->number)
-		return (1);
-	return (0);
 }
 
 int		philo_live(t_board *board, t_philo *philo, int id)
@@ -57,15 +37,10 @@ int		philo_live(t_board *board, t_philo *philo, int id)
 	philo_speak(board, philo, "is eating\n", id);
 	usleep_custom(board->eat);
 	philo[id].count_meal++;
-	if (philo[id].count_meal == board->limit)
-		board->full_number++;
-	if (board->full_number == board->number)
-		board->stop = 1;
-	philo[id].last_meal = get_time();
+	philo[id].last_meal = get_time();	
 	// Puting forks back
 	pthread_mutex_unlock(philo[id].l_fork);
 	pthread_mutex_unlock(philo[id].r_fork);
-
 
 	// Sleep
 	philo_speak(board, philo, "is sleeping\n", id);
@@ -73,6 +48,8 @@ int		philo_live(t_board *board, t_philo *philo, int id)
 
 	// Think
 	philo_speak(board, philo, "is thinking\n", id);
+	if (board->limit != 1 && philo[id].count_meal >= board->limit)
+		return (1);
 	return (0);
 }
 
